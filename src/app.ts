f2a0155c078c891eb3f396298749b6422bbd9b72
lhelheo <leonardo.oliveira.villalva@hotmail.com>
2025-07-03 12:21:52 -0300

    feat: testing

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
-
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
@@ -13,5 +10,4 @@ app.use(express.json());
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
 });
-
 export default app;
